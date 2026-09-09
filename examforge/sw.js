669cda6544df56a29605200208a3321fec1e9e4c
Champak Roy <champaksworld@gmail.com>
2026-09-09 18:15:40 +0530

    Cache exam clock assets and refresh PWA

--- examforge/sw.js
+++ examforge/sw.js
@@ -1,5 +1,5 @@
-const CACHE='examforge-demo-v5';
-const ASSETS=['./','index.html','styles.css','test.css','app.js','test-engine.js','manifest.json','icon.svg'];
+const CACHE='examforge-demo-v6';
+const ASSETS=['./','index.html','styles.css','test.css','exam-clock.css','app.js','test-engine.js','exam-clock.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{
