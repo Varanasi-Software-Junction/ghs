eb373a18ad07c3f91407274fbc992c8d2f713a8c
Champak Roy <champaksworld@gmail.com>
2026-09-12 12:11:42 +0530

    Refresh ExamForge clock assets

--- examforge/sw.js
+++ examforge/sw.js
@@ -1,4 +1,4 @@
-const CACHE='examforge-demo-v6';
+const CACHE='examforge-demo-v7';
 const ASSETS=['./','index.html','styles.css','test.css','exam-clock.css','app.js','test-engine.js','exam-clock.js','manifest.json','icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
